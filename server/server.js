512a99756ac3f933660a970d297ede95cf6e32c9
Henrique Sbrissia <henriqueerics@gmail.com>
2024-10-11 02:44:18 -0300

    aply cors

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import fastify from 'fastify';
+import cors from '@fastify/cors';
 import userRoutes from './routes/userRoutes.js';
 import jwt from '@fastify/jwt';
 
 const app = fastify({ logger: true });
+
+await app.register(cors);
 
 app.register(jwt, {
   secret: process.env.JWT_SECRET,
