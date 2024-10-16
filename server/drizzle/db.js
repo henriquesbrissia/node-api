21aa975f003eb715030bd3d11e320119e7e3ba8f
Henrique Sbrissia <henriqueerics@gmail.com>
2024-10-16 13:59:39 -0300

    Update db.js

--- server/drizzle/db.js
+++ server/drizzle/db.js
@@ -1,7 +1,8 @@
+import * as schema from './schema';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
 const sql = postgres(process.env.DATABASE_URL);
-const db = drizzle(sql);
+const db = drizzle(sql, { schema });
 
 export default db;
