4199e6e41d4006ce7684dc23488377042968a938
Henrique Sbrissia <henriqueerics@gmail.com>
2024-10-18 03:30:37 -0300

    Remove middleware from sign-in

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -8,7 +8,7 @@ import { protect } from '../middlewares/userMiddleware.js';
 
 async function userRoutes(app, options) {
   app.post('/sign-up', registerUser);
-  app.post('/sign-in', { preValidation: [protect] }, loginUser);
+  app.post('/sign-in', loginUser);
   app.post('/forgot-password', requestPasswordReset);
   app.post('/reset-password', { preValidation: [protect] }, resetPassword);
 }
